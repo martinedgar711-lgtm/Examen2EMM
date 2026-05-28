b6cfbf83adc1b49368d258fe784e42fabe1c6974
edgar <martinedgar711@gmail.com>
2026-05-27 21:17:38 -0600

    Paso de nombre de usuario

--- app/src/main/java/com/example/examen2emm/ProfileActivity.kt
+++ app/src/main/java/com/example/examen2emm/ProfileActivity.kt
@@ -1,4 +1,19 @@
 package com.example.examen2emm
 
-class ProfileActivity {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val txtBienvenida = findViewById<TextView>(R.id.txtBienvenida)
+
+        val usuario = intent.getStringExtra("usuario")
+
+        txtBienvenida.text = "Bienvenido $usuario"
+    }
 }
