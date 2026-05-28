a691d46fe7dd813aecb396456752e9edb5ae41fc
edgar <martinedgar711@gmail.com>
2026-05-27 21:11:55 -0600

    Validación de contraseña

--- app/src/main/java/com/example/examen2emm/MainActivity.kt
+++ app/src/main/java/com/example/examen2emm/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.examen2emm
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val edtUsuario = findViewById<EditText>(R.id.edtUsuario)
+        val edtPassword = findViewById<EditText>(R.id.edtPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+
+            val password = edtPassword.text.toString()
+
+            if(password == "abc123") {
+
+                val usuario = edtUsuario.text.toString()
+
+                val intent = Intent(this, ProfileActivity::class.java)
+
+                intent.putExtra("usuario", usuario)
+
+                startActivity(intent)
+
+            } else {
+
+                Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
